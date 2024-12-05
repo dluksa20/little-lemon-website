d92c2d137719507fbeb6be6d626f8f0029f9d6eb
Dovydas Luksa <dluksa20@gmail.com>
2024-12-05 22:48:40 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from './components/header/Header.js'
 
 function App() {
   return (
-    <>
+    <div className='main'>
       <Header/>
       <Footer/>
-    </>
+    </div>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
         <ul className="links">
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><a href="/services">Menu</a></li>
+          <li><a href="/contact">Reserve a Table</a></li>
         </ul>
       </nav>
     </header>
